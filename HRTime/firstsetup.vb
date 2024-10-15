9f56aed0ebd6f146a487cb853c28e42aa3dd333d
ayukovt <ayukovt@proton.me>
2024-10-16 00:08:18 +0200

    im a silly pupy i cant cod help >.<

--- HRTime/firstsetup.vb
+++ HRTime/firstsetup.vb
@@ -10,7 +10,7 @@
         Else
             MaterialTabControl1.SelectedTab = TabPage3
         End If
-        If MetroTextBox1.Text = "passoid" Then
+        If LCase(MetroTextBox1.Text) = "passoid" Or LCase(MetroTextBox1.Text) = "hon" Or LCase(MetroTextBox1.Text) = "gigahon" Or LCase(MetroTextBox1.Text) = "ogrehon" Or LCase(MetroTextBox1.Text) = "ogre" Or LCase(MetroTextBox1.Text) = "boymoder" Or LCase(MetroTextBox1.Text) = "iwnbaw" Or LCase(MetroTextBox1.Text) = "ywnbaw" Or LCase(MetroTextBox1.Text) = "ngmi" Or LCase(MetroTextBox1.Text) = "hsts" Then
             MessageBox.Show("please get off 4chan. for your own good.", "HRTime",
             MessageBoxButtons.OK, MessageBoxIcon.Error)
             MaterialTabControl1.SelectedTab = TabPage3
